375f3cf9b881b58651d526c7eff5636689a575da
Lukasz Swiderski <lukasz@swiderski.xyz>
2015-12-07 20:26:08 +0000

    movement with boxes

--- Level.py
+++ Level.py
@@ -28,20 +28,16 @@ class Level:
 
     def playerMoveUp(self):
         p = self.player.sprites()[0]
-        if(self.isWallOnPlace( p.x, p.y-1)==False):
-            p.moveUp()
+        self.tryMove(p.x,p.y,p.x,p.y-1)
     def playerMoveDown(self):
         p = self.player.sprites()[0]
-        if(self.isWallOnPlace( p.x, p.y+1)==False):
-            p.moveDown()
+        self.tryMove(p.x,p.y,p.x,p.y+1)
     def playerMoveLeft(self):
         p = self.player.sprites()[0]
-        if(self.isWallOnPlace( p.x-1, p.y)==False):
-            p.moveLeft()
+        self.tryMove(p.x,p.y,p.x-1,p.y)
     def playerMoveRight(self):
         p = self.player.sprites()[0]
-        if(self.isWallOnPlace( p.x+1, p.y)==False):
-            p.moveRight()
+        self.tryMove(p.x,p.y,p.x+1,p.y)
 
     def isBoxOnPlace(self, x, y):
         for i in self.box_list.sprites():
@@ -56,6 +52,48 @@ class Level:
                 if(i.type=="#"):
                     return True
         return False
+
+    def isFree(self,x,y):
+        if(self.isWallOnPlace(x,y)==False and self.isBoxOnPlace(x,y)==False):
+            return True
+        else:
+            return False
+
+    def tryMove(self,fromX,fromY,toX,toY):
+        dX = toX - fromX
+        dY = toY - fromY
+        if(self.isWallOnPlace(toX, toY)==True):
+            return False
+        elif(self.isBoxOnPlace(toX, toY)==True):
+            if(self.isWallOnPlace(toX + dX, toY + dY)):
+                return False
+            elif(self.isBoxOnPlace(toX + dX, toY + dY)):
+                return False
+            else:
+                #you can move box there
+                #and move yourself on box place
+                self.moveBox(toX,toY,dX,dY)
+                self.movePlayer(dX,dY)
+        else:
+            #justmove to place
+            self.movePlayer(dX,dY)
+
+    def movePlayer(self,dx,dy):
+        p = self.player.sprites()[0]
+
+        if(dx==0 and dy==-1):
+            p.moveUp()
+        elif(dx==0 and dy==1):
+            p.moveDown()
+        elif(dx==-1 and dy==0):
+            p.moveLeft()
+        elif(dx==1 and dy==0):
+            p.moveRight()
+
+    def moveBox(self,x,y,dx,dy):
+         for i in self.box_list.sprites():
+            if( i.x==x and i.y==y):
+                i.changePositin(x+dx,y+dy)
 
 
 
